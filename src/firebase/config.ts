93021af819e7df4c721ba0517472f497a8e9fcd8
Harini R <harinirnr3@gmail.com>
2026-03-17 15:11:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3908161200-95ef2",
+  "appId": "1:262386298381:web:aae13458148d27ec49af44",
+  "apiKey": "AIzaSyC2LAwROmLBS-tT3ftcA0UoZkgMGzLhO2U",
+  "authDomain": "studio-3908161200-95ef2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "262386298381"
+};
